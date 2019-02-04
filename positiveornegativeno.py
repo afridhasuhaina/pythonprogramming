fcdba5193f4cd269a78bbd09856c686515c38924
afridhasuhaina <47314310+afridhasuhaina@users.noreply.github.com>
2019-02-04 15:19:38 +0530

    Update positiveornegativeno.py

--- positiveornegativeno.py
+++ positiveornegativeno.py
@@ -1,7 +1,7 @@
-a=input("enter the number:")
+a=input()
 if(a>0):
-print("number is positive")
+print("Positive")
 elif(a<0):
-print("number is negative")
+print("Negative")
 else:
-print("number is zero")
+print("Zero")
