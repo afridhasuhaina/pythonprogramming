3c637c0f065957efe96b102d888193a30a1799fd
afridhasuhaina <47314310+afridhasuhaina@users.noreply.github.com>
2019-02-06 11:34:30 +0530

    Update vowelorconsonant.py

--- vowelorconsonant.py
+++ vowelorconsonant.py
@@ -1,5 +1,7 @@
 ch = input()
-if((ch =="a" or ch=="A" )or (ch=="z"or ch=="Z")):
+if(ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch="u"):
+  print("Vowel")
+elif((ch =="a" or ch=="A" )or (ch=="z"or ch=="Z")):
   print("Consonant")
 else:
   print("invalid")
